5c7fba14f96d3e43f33a78b5e2bd30b9861ffecb
Giuseppe <giuseppe.ilsami@gmail.com>
2021-03-23 17:37:52 +0100

    game 99% done

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,75 +1,112 @@
 let arrayOne = ["Scissors", "Paper", "Rock", "Lizard", "Spock"];
 let arrayTwo = arrayOne;
 let attribution = document.querySelector(".attribution");
-let playAgainBtn = document.querySelector("button");
+if (window.localStorage.getItem("score") === null) {
+  window.localStorage.setItem("score", 0);
+}
+
+let addScore = (plusThis) => {
+  let score = JSON.parse(window.localStorage.getItem("score"));
+  window.localStorage.setItem("score", score + plusThis);
+  let scoreSpan = document.querySelector(".score span");
+  scoreSpan.innerHTML = score;
+};
+
+addScore(+0);
 
 let winLose = () => {
   let youPicked = document.querySelector("button.you-picked");
-  let pcPicked = document.querySelector("span.pc-picked");
-  let score = null;
+  let housePicked = document.querySelector("span.house-picked");
   switch (true) {
-    case youPicked.innerHTML === pcPicked.innerHTML:
-      alert("+0");
+    case youPicked.innerHTML === housePicked.innerHTML:
+      addScore(+0);
+      alert("pareggio");
       break;
-    case youPicked.innerHTML === "Scissors" && pcPicked.innerHTML === "Paper":
-      alert("+1");
+    case youPicked.innerHTML === "Scissors" &&
+      housePicked.innerHTML === "Paper":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Paper" && pcPicked.innerHTML === "Scissors":
-      alert("-1");
+    case youPicked.innerHTML === "Paper" &&
+      housePicked.innerHTML === "Scissors":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Paper" && pcPicked.innerHTML === "Rock":
-      alert("+1");
+    case youPicked.innerHTML === "Paper" && housePicked.innerHTML === "Rock":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Rock" && pcPicked.innerHTML === "Paper":
-      alert("-1");
+    case youPicked.innerHTML === "Rock" && housePicked.innerHTML === "Paper":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Rock" && pcPicked.innerHTML === "Lizard":
-      alert("+1");
+    case youPicked.innerHTML === "Rock" && housePicked.innerHTML === "Lizard":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Lizard" && pcPicked.innerHTML === "Rock":
-      alert("-1");
+    case youPicked.innerHTML === "Lizard" && housePicked.innerHTML === "Rock":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Lizard" && pcPicked.innerHTML === "Spock":
-      alert("+1");
+    case youPicked.innerHTML === "Lizard" && housePicked.innerHTML === "Spock":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Spock" && pcPicked.innerHTML === "Lizard":
-      alert("-1");
+    case youPicked.innerHTML === "Spock" && housePicked.innerHTML === "Lizard":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Spock" && pcPicked.innerHTML === "Scissors":
-      alert("+1");
+    case youPicked.innerHTML === "Spock" &&
+      housePicked.innerHTML === "Scissors":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Scissors" && pcPicked.innerHTML === "Spock":
-      alert("-1");
+    case youPicked.innerHTML === "Scissors" &&
+      housePicked.innerHTML === "Spock":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Scissors" && pcPicked.innerHTML === "Lizard":
-      alert("+1");
+    case youPicked.innerHTML === "Scissors" &&
+      housePicked.innerHTML === "Lizard":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Lizard" && pcPicked.innerHTML === "Scissors":
-      alert("-1");
+    case youPicked.innerHTML === "Lizard" &&
+      housePicked.innerHTML === "Scissors":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Paper" && pcPicked.innerHTML === "Spock":
-      alert("+1");
+    case youPicked.innerHTML === "Paper" && housePicked.innerHTML === "Spock":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Spock" && pcPicked.innerHTML === "Paper":
-      alert("-1");
+    case youPicked.innerHTML === "Spock" && housePicked.innerHTML === "Paper":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Rock" && pcPicked.innerHTML === "Scissors":
-      alert("+1");
+    case youPicked.innerHTML === "Rock" && housePicked.innerHTML === "Scissors":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Scissors" && pcPicked.innerHTML === "Rock":
-      alert("-1");
+    case youPicked.innerHTML === "Scissors" && housePicked.innerHTML === "Rock":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Lizard" && pcPicked.innerHTML === "Paper":
-      alert("+1");
+    case youPicked.innerHTML === "Lizard" && housePicked.innerHTML === "Paper":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Paper" && pcPicked.innerHTML === "Lizard":
-      alert("-1");
+    case youPicked.innerHTML === "Paper" && housePicked.innerHTML === "Lizard":
+      addScore(-1);
+      alert("perso");
       break;
-    case youPicked.innerHTML === "Spock" && pcPicked.innerHTML === "Rock":
-      alert("+1");
+    case youPicked.innerHTML === "Spock" && housePicked.innerHTML === "Rock":
+      addScore(+1);
+      alert("vinto");
       break;
-    case youPicked.innerHTML === "Rock" && pcPicked.innerHTML === "Spock":
-      alert("-1");
+    case youPicked.innerHTML === "Rock" && housePicked.innerHTML === "Spock":
+      addScore(-1);
+      alert("perso");
       break;
     default:
       alert("Qualcosa è andato storto...");
@@ -77,13 +114,27 @@ let winLose = () => {
   }
 };
 
-let pcPicked = (arr) => {
-  let pcPicked = document.createElement("span");
+let housePicked = (arr) => {
+  let housePicked = document.createElement("span");
   let localPicked = arr[Math.floor(Math.random() * arr.length)];
-  pcPicked.textContent = localPicked;
-  pcPicked.classList.add("pc-picked");
+  housePicked.textContent = localPicked;
+  housePicked.classList.add("house-picked");
   let youPicked = document.querySelector("button.you-picked");
-  youPicked.insertAdjacentElement("afterend", pcPicked);
+  youPicked.insertAdjacentElement("afterend", housePicked);
+  let playAgainBtn = document.createElement("button");
+  playAgainBtn.className = "play-again";
+  playAgainBtn.textContent = "Play Again";
+  housePicked.after(playAgainBtn);
+  playAgainBtn.onclick = (e) => {
+    let yourPick = document.querySelector("button.you-picked");
+    let housePick = document.querySelector("span.house-picked");
+    if (yourPick.disabled === true) {
+      yourPick.remove();
+      housePick.remove();
+      buildBtns(arrayOne);
+      playAgainBtn.remove();
+    }
+  };
 };
 
 let youPicked = (e) => {
@@ -96,8 +147,9 @@ let youPicked = (e) => {
       btn.remove();
     }
   }
-  pcPicked(arrayTwo);
+  housePicked(arrayTwo);
   winLose();
+  addScore(+0);
 };
 
 let buildBtns = (arr) => {
